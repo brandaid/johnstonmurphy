bf2738ccb197a45e9bbe35554d3cfbc431492932
Matias Jaubet <matias@jaubet.com>
2016-06-30 11:29:41 -0300

    To box-4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="es">
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Johnston & Murphy</title>
@@ -20,17 +20,73 @@
 	<![endif]-->
 </head>
 <body>
-	<header class="header">
-		header
-	</header>
 	<main>
 		<div class="container">
-			contain
+			
+			<!--BOX 1-->
+			<section class="box-1">
+				<div class="caption">
+					<h1>START WITH <br>THE SHOES</h1>
+					<p>Foundations for <br>a Well-Lived Life</p>
+				</div>
+			</section>
+			
+			<!--BOX 2-->
+			<section class="box-2">
+				<div class="box-data">
+					<h2>FOOTWEAR FIRST</h2>
+					<p>Our style philosophy starts from the ground up.</p>
+					<p>Building a timeless wardrobe around classic, versatile pieces that you can dress up or down, transcending trends and giving you a foundation of effortless style that lasts.</p>
+					<p>We believe when you’re on solid footing, the rest comes easy.</p>
+				</div>
+				<div class="box-image">
+					<img src="images/box-2.jpg" alt="">
+				</div>
+			</section>
+
+			<!--BOX 3-Video-->
+			<section class="box-3-video">
+				<a href=""><img src="images/video.jpg" alt=""></a>
+			</section>
+
+			<!--BOX 4-->
+			<section class="box-4">
+				<div class="box-data">
+					<h2>A Chic-er Sneaker</h2>
+					<p>Multi-task in style. The effortless go-to for the girl on the go.</p>
+					<a href="" class="btn">SHOP THE EMERSON</a>
+				</div>
+				<div class="box-image">
+					<img src="images/box-4.jpg" alt="">
+				</div>
+			</section>
+			<section class="box-4-images">
+				<div class="box-two-img first">
+					<img src="images/box-4-2.jpg" alt="">
+				</div>
+				<div class="box-two-img last">
+					<img src="images/box-4-3.jpg" alt="">
+				</div>
+			</section>
+
+			<!--BOX 4-->
+			<section class="box-5">
+				<div class="caption">
+					<small>style philosophy</small>
+					<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
+				</div>
+			</section>
+
+			<!--BOX 5-->
+			<section class="box-5">
+				<img src="images/box-5.jpg" alt="">
+				<div class="caption">
+					<small>style philosophy</small>
+					<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
+				</div>
+			</section>
 		</div>
 	</main>
-	<footer class="footer">
-		footer
-	</footer>
 	<!--JQUERY -->
 	<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
 
-	//GALERIA DE IMAGENES
-	$(".lightgallery").lightGallery(); 
-
-	$('.launchGallery').click(function(){
-	    $(this).siblings('div.lightgallery').find("a").trigger('click');    
-	});
 	
 });
