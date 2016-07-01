b4e35e4110e9fdc53f2851cf22bda2885d57e88e
Matias Jaubet <matias@jaubet.com>
2016-07-01 15:17:17 -0300

    Re-ordening the landings

---
+++ landing-2/index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Johnston & Murphy</title>
+	
+	<meta name="description" content="">
+	<meta name="revisit-after" content="30 days">
+	<meta name="robots" content="index,follow">
+	<meta name="distribution" content="global">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="js/respond.min.js"></script>
+	  <script src="js/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<main>
+		<div class="container">
+			
+			<!--BOX 1-->
+			<section class="box-1">
+				<div class="caption">
+					<small>Introducing</small>
+					<h2>HAPPILY GREY</h2>
+					<img src="images/box-1-mobile.jpg" alt="" class="mobile">
+					<p>We caught up with the style blogger and hometown Nashville girl crush to find out her perspective on life, style, and of course, the shoes.</p>
+				</div>
+				<img src="images/box-1.jpg" alt="" class="desktop">
+			</section>
+			
+			<!--BOX 2-->
+			<section class="box-2">
+				<div class="box-data">
+					<h2>MY STYLE <br>FOUNDATIONS</h2>
+				</div>
+			</section>
+			
+			<section class="box-2-images">
+				<div class="box-two-img first">
+					<img src="images/box-2-bottom.jpg" alt="" class="in-desktop">
+					<img src="images/box-2.jpg" alt="" class="vertical">
+				</div>
+				<div class="box-two-img last">
+					<img src="images/box-2-1.jpg" alt="">
+					<p class="in-desktop">"I enjoy putting looks together <br>side-by-side, rarely do I wear one <br> piece, one way. </p>
+					<p class="in-desktop">Style is meant to be utilitarian. <br>It’s expression, yes, but it’s evolving.</p>
+					<p class="in-desktop">I’m no expert, but my style allows <br> me to share all that inspires me."</p>
+				</div>
+			</section>
+
+			<section class="box-2-bottom">
+				<div class="box-data in-mobile">
+					<p>"I enjoy putting looks together side-by-side, rarely do I wear one piece, one way. Style is meant to be utilitarian. It’s expression, yes, but it’s evolving. I’m no expert, but my style allows me to share all that inspires me."</p>
+				</div>
+				<div class="box-image">
+					<img src="images/box-2-bottom.jpg" alt="">
+				</div>
+			</section>
+
+			<!--BOX 3-->
+			<section class="box-3">
+				<h2>THE FALL EDIT</h2>
+				<small>by Happily Grey</small>
+			</section>
+
+			<section class="box-3-noticia">
+				<div class="box-image">
+					<img src="images/box-3-1.jpg" alt="">
+				</div>
+				<div class="box-data">
+					<h2>THE EMERSON</h2>
+					<p>"I want these in every color. I love the texture of a well-worn sneaker. It has an off-duty feel."</p>
+				</div>
+			</section>
+			<section class="box-3-noticia float">
+				<div class="box-data">
+					<h2>THE GWYNN</h2>
+					<p>"I love those borrowed-from-the-boys vibes. I’ve decided I don’t wear enough loafers; I forgot how versatile they are."</p>
+				</div>
+				<div class="box-image">
+					<img src="images/box-3-2.jpg" alt="">
+				</div>
+			</section>
+			<section class="box-3-noticia">
+				<div class="box-image">
+					<img src="images/box-3-3.jpg" alt="">
+				</div>
+				<div class="box-data">
+					<h2>THE STEPHANIE</h2>
+					<p>"Glad to see midis stuck around for fall, updated with icy blues and greys. That leather will only get better with age."</p>
+				</div>
+			</section>
+
+
+			
+		</div>
+	</main>
+	<!--JQUERY -->
+	<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
