2e66fb7cac7e4ce32e5b07e9d97fd9c2ae4a5cae
Matias Jaubet <matias@jaubet.com>
2016-08-01 16:35:35 -0300

    final landing with container

--- blogger-landing/index.php
+++ blogger-landing/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="html-blogger-landing ">
 <head>
 	<meta charset="UTF-8">
 	<title>Johnston & Murphy</title>
@@ -20,164 +20,165 @@
 	<![endif]-->
 </head>
 <body>
-	<main>
-		<div class="container">
-			<header class="header">
-				<div class="box-legend">
-					<p>Free Shipping on Orders Over $100 + Free Returns. <a href="">Details</a></p>
-				</div>
-				<div class="table-header">
-					<div class="col col-menu">
-						<a href=""><img src="images/ico-menu.png" alt="Icon menu"></a>
+	<main class="body-blogger-landing">
+		<div class="blogger-landing">
+			<div class="container">
+				<header class="header">
+					<div class="box-legend">
+						<p>Free Shipping on Orders Over $100 + Free Returns. <a href="">Details</a></p>
 					</div>
-					<div class="col col-locate">
-						<a href=""><img src="images/ico-locate.png" alt="Location"></a>
+					<div class="table-header">
+						<div class="col col-menu">
+							<a href=""><img src="images/ico-menu.png" alt="Icon menu"></a>
+						</div>
+						<div class="col col-locate">
+							<a href=""><img src="images/ico-locate.png" alt="Location"></a>
+						</div>
+						<div class="col col-brand">
+							<a href=""><img src="images/brand.png" alt="Johnston & Murphy"></a>
+						</div>
+						<div class="col col-search">
+							<a href=""><img src="images/ico-search.png" alt="Search"></a>
+						</div>
+						<div class="col col-bag">
+							<img src="images/ico-bag.png" alt="Bag">
+						</div>
 					</div>
-					<div class="col col-brand">
-						<a href=""><img src="images/brand.png" alt="Johnston & Murphy"></a>
+				</header>
+			</div>
+			<div class="container">
+				
+				<!--BOX 1-->
+				<section class="box-1">
+					<div class="caption">
+						<small>Introducing</small>
+						<h2>HAPPILY GREY</h2>
+						<a href="#" class="image-link"><img src="images/box-1-mobile.jpg" alt="Happly Grey" class="mobile"></a>
+						<p>We caught up with the style blogger and hometown Nashville girl crush to find out her perspective on life, style, and of course, the shoes.</p>
 					</div>
-					<div class="col col-search">
-						<a href=""><img src="images/ico-search.png" alt="Search"></a>
+					<a href="#" class="image-link"><img src="images/box-1.jpg" alt="Happly Grey" class="desktop"></a>
+				</section>
+				
+				<!--BOX 2-->
+				<section class="box-2">
+					<div class="box-data">
+						<h2>MY STYLE <br>FOUNDATIONS</h2>
 					</div>
-					<div class="col col-bag">
-						<img src="images/ico-bag.png" alt="Bag">
+				</section>
+				
+				<section class="box-2-images">
+					<div class="box-two-img first">
+						<a href="#" class="image-link"><img src="images/box-2-bottom.jpg" alt="My Style Foundations" class="in-desktop"></a>
+						<a href="#" class="image-link"><img src="images/box-2.jpg" alt="My Style Foundations" class="vertical"></a>
 					</div>
-				</div>
-			</header>
-		</div>
-		<div class="container">
-			
-			<!--BOX 1-->
-			<section class="box-1">
-				<div class="caption">
-					<small>Introducing</small>
-					<h2>HAPPILY GREY</h2>
-					<a href="#" class="image-link"><img src="images/box-1-mobile.jpg" alt="Happly Grey" class="mobile"></a>
-					<p>We caught up with the style blogger and hometown Nashville girl crush to find out her perspective on life, style, and of course, the shoes.</p>
-				</div>
-				<a href="#" class="image-link"><img src="images/box-1.jpg" alt="Happly Grey" class="desktop"></a>
-			</section>
-			
-			<!--BOX 2-->
-			<section class="box-2">
-				<div class="box-data">
-					<h2>MY STYLE <br>FOUNDATIONS</h2>
-				</div>
-			</section>
-			
-			<section class="box-2-images">
-				<div class="box-two-img first">
-					<a href="#" class="image-link"><img src="images/box-2-bottom.jpg" alt="My Style Foundations" class="in-desktop"></a>
-					<a href="#" class="image-link"><img src="images/box-2.jpg" alt="My Style Foundations" class="vertical"></a>
-				</div>
-				<div class="box-two-img last">
-					<a href="#" class="image-link"><img src="images/box-2-1.jpg" alt="My Style Foundations"></a>
-					<p class="in-desktop">“I enjoy putting looks together <br>side-by-side, rarely do I wear one <br> piece, one way. </p>
-					<p class="in-desktop">Style is meant to be utilitarian. <br>It’s expression, yes, but it’s evolving.</p>
-					<p class="in-desktop">I’m no expert, but my style allows <br> me to share all that inspires me.”</p>
-				</div>
-			</section>
+					<div class="box-two-img last">
+						<a href="#" class="image-link"><img src="images/box-2-1.jpg" alt="My Style Foundations"></a>
+						<p class="in-desktop">“I enjoy putting looks together <br>side-by-side, rarely do I wear one <br> piece, one way. </p>
+						<p class="in-desktop">Style is meant to be utilitarian. <br>It’s expression, yes, but it’s evolving.</p>
+						<p class="in-desktop">I’m no expert, but my style allows <br> me to share all that inspires me.”</p>
+					</div>
+				</section>
 
-			<section class="box-2-bottom">
-				<div class="box-data in-mobile">
-					<p>“I enjoy putting looks together side-by-side, rarely do I wear one piece, one way. Style is meant to be utilitarian. It’s expression, yes, but it’s evolving. I’m no expert, but my style allows me to share all that inspires me.”</p>
-				</div>
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-2-bottom.jpg" alt="My Style Foundations"></a>
-				</div>
-			</section>
-
-			<!--BOX 3-->
-			<section class="box-3">
-				<h2>THE FALL EDIT</h2>
-				<small>by Happily Grey</small>
-			</section>
-
-			<section class="box-3-noticia">
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-3-1.jpg" alt="The Fall Edit"></a>
-				</div>
-				<div class="box-data">
-					<h2>THE EMERSON</h2>
-					<p>“I want these in every color. I love the texture of a well-worn sneaker. It has an off-duty feel.”</p>
-				</div>
-			</section>
-			<section class="box-3-noticia float">
-				<div class="box-data">
-					<h2>THE GWYNN</h2>
-					<p>“I love those borrowed-from-the-boys vibes. I’ve decided I don’t wear enough loafers; I forgot how versatile they are.”</p>
-				</div>
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-3-2.jpg" alt="The Fall Edit"></a>
-				</div>
-			</section>
-			<section class="box-3-noticia">
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-3-3.jpg" alt="The Fall Edit"></a>
-				</div>
-				<div class="box-data">
-					<h2>THE STEPHANIE</h2>
-					<p>“Glad to see midis stuck around for fall, updated with icy blues and greys. That leather will only get better with age.”</p>
-				</div>
-			</section>
-
-			
-			<!--BOX 4-->
-			<section class="box-4">
-				<h2>SHOP THE EDIT</h2>
-				<div class="shop">
-					<div class="col">
-						<a href="http://www.johnstonmurphy.com/emerson-sneaker/6951.html?dwvar_6951_width=M&dwvar_6951_color=Off%20White%20Italian%20Snake%20Print%20Leather#start=3"><img src="images/shop-1.jpg" alt=""></a>
-						<h4>Emerson Sneaker</h4>
+				<section class="box-2-bottom">
+					<div class="box-data in-mobile">
+						<p>“I enjoy putting looks together side-by-side, rarely do I wear one piece, one way. Style is meant to be utilitarian. It’s expression, yes, but it’s evolving. I’m no expert, but my style allows me to share all that inspires me.”</p>
 					</div>
-					<div class="col">
-						<a href="http://www.johnstonmurphy.com/gwynn-penny/6946.html?dwvar_6946_width=M&dwvar_6946_color=Bordeaux%20Textured%20Patent%20Leather#start=2"><img src="images/shop-2.jpg" alt=""></a>
-						<h4>Gwynn Penny</h4>
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-2-bottom.jpg" alt="My Style Foundations"></a>
 					</div>
-					<div class="col">
-						<a href="http://www.johnstonmurphy.com/stephanie-stitched-bootie/6947.html?dwvar_6947_color=Gray%20Italian%20Suede&dwvar_6947_width=M#start=1"><img src="images/shop-3.jpg" alt=""></a>
-						<h4>Stephanie Stitched Bootie</h4>
-					</div>
-				</div>
-				<a href="" class="btn">SEE MORE</a>
-			</section>
-			
-			<!-- BOX 5 TESTIMONIAL-->
-			<section class="box-5-testimonial">
-				<h3>The way one walks, talks and stands all create style, but none of it's possible without personal confidence.</h3>
-				<p>MARY SENG @HappilyGrey</p>
-			</section>
-			
-			<!-- BOX 6 -->
-			<section class="box-6">
-				<h2>ONE BOOT</h2>
-				<span></span>
-				<h2>TWO WAYS</h2>
-			</section>
+				</section>
 
-			<section class="box-6-one">
-				<div class="col-left">
-					<a href="#" class="image-link"><img src="images/box-6.jpg" alt=""></a>
-				</div>
-				<div class="col-right">
-					<p class="number">1.</p>
-					<a href="#" class="image-link"><img src="images/box-6-1.jpg" alt="" class="desktop"></a>
-					<p class="text">“Safe to say these boots go with everything. I like them best with a knee-rip skinny, an oversized knit, and a topknot. Cozy casual, but still put-together.”</p>
-				</div>
-			</section>
-	
-			<section class="box-6-two">
-				<div class="col-left">
-					<p class="number">2.</p>
-					<a href="#" class="image-link"><img src="images/box-6-3.jpg" alt="" class="desktop"></a>
-					<p class="text">“I like where these hit. Mom was right…Always show some ankle, it’s the littlest part of you.”</p>
-				</div>
-				<div class="col-right">
-					<a href="#" class="image-link"><img src="images/box-6-2.jpg" alt=""></a>
-				</div>
-				<a href="" class="btn">SHOP THE STEPHANIE</a>
-			</section>
+				<!--BOX 3-->
+				<section class="box-3">
+					<h2>THE FALL EDIT</h2>
+					<small>by Happily Grey</small>
+				</section>
+
+				<section class="box-3-noticia">
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-3-1.jpg" alt="The Fall Edit"></a>
+					</div>
+					<div class="box-data">
+						<h2>THE EMERSON</h2>
+						<p>“I want these in every color. I love the texture of a well-worn sneaker. It has an off-duty feel.”</p>
+					</div>
+				</section>
+				<section class="box-3-noticia float">
+					<div class="box-data">
+						<h2>THE GWYNN</h2>
+						<p>“I love those borrowed-from-the-boys vibes. I’ve decided I don’t wear enough loafers; I forgot how versatile they are.”</p>
+					</div>
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-3-2.jpg" alt="The Fall Edit"></a>
+					</div>
+				</section>
+				<section class="box-3-noticia">
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-3-3.jpg" alt="The Fall Edit"></a>
+					</div>
+					<div class="box-data">
+						<h2>THE STEPHANIE</h2>
+						<p>“Glad to see midis stuck around for fall, updated with icy blues and greys. That leather will only get better with age.”</p>
+					</div>
+				</section>
+
+				
+				<!--BOX 4-->
+				<section class="box-4">
+					<h2>SHOP THE EDIT</h2>
+					<div class="shop">
+						<div class="col">
+							<a href="http://www.johnstonmurphy.com/emerson-sneaker/6951.html?dwvar_6951_width=M&dwvar_6951_color=Off%20White%20Italian%20Snake%20Print%20Leather#start=3"><img src="images/shop-1.jpg" alt=""></a>
+							<h4>Emerson Sneaker</h4>
+						</div>
+						<div class="col">
+							<a href="http://www.johnstonmurphy.com/gwynn-penny/6946.html?dwvar_6946_width=M&dwvar_6946_color=Bordeaux%20Textured%20Patent%20Leather#start=2"><img src="images/shop-2.jpg" alt=""></a>
+							<h4>Gwynn Penny</h4>
+						</div>
+						<div class="col">
+							<a href="http://www.johnstonmurphy.com/stephanie-stitched-bootie/6947.html?dwvar_6947_color=Gray%20Italian%20Suede&dwvar_6947_width=M#start=1"><img src="images/shop-3.jpg" alt=""></a>
+							<h4>Stephanie Stitched Bootie</h4>
+						</div>
+					</div>
+					<a href="" class="btn">SEE MORE</a>
+				</section>
+				
+				<!-- BOX 5 TESTIMONIAL-->
+				<section class="box-5-testimonial">
+					<h3>The way one walks, talks and stands all create style, but none of it's possible without personal confidence.</h3>
+					<p>MARY SENG @HappilyGrey</p>
+				</section>
+				
+				<!-- BOX 6 -->
+				<section class="box-6">
+					<h2>ONE BOOT</h2>
+					<span></span>
+					<h2>TWO WAYS</h2>
+				</section>
+
+				<section class="box-6-one">
+					<div class="col-left">
+						<a href="#" class="image-link"><img src="images/box-6.jpg" alt=""></a>
+					</div>
+					<div class="col-right">
+						<p class="number">1.</p>
+						<a href="#" class="image-link"><img src="images/box-6-1.jpg" alt="" class="desktop"></a>
+						<p class="text">“Safe to say these boots go with everything. I like them best with a knee-rip skinny, an oversized knit, and a topknot. Cozy casual, but still put-together.”</p>
+					</div>
+				</section>
 		
+				<section class="box-6-two">
+					<div class="col-left">
+						<p class="number">2.</p>
+						<a href="#" class="image-link"><img src="images/box-6-3.jpg" alt="" class="desktop"></a>
+						<p class="text">“I like where these hit. Mom was right…Always show some ankle, it’s the littlest part of you.”</p>
+					</div>
+					<div class="col-right">
+						<a href="#" class="image-link"><img src="images/box-6-2.jpg" alt=""></a>
+					</div>
+					<a href="" class="btn">SHOP THE STEPHANIE</a>
+				</section>
+			</div>
 		</div>
 	</main>
 	<!--JQUERY -->
--- womens-landing/index.php
+++ womens-landing/index.php
@@ -19,166 +19,168 @@
 	  <script src="js/html5.js"></script>
 	<![endif]-->
 </head>
-<body class="womens-landing">
+<body>
 	<main>
-		<div class="container">
-			<header class="header">
-				<div class="box-legend">
-					<p>Free Shipping on Orders Over $100 + Free Returns. <a href="">Details</a></p>
-				</div>
-				<div class="table-header">
-					<div class="col col-menu">
-						<a href=""><img src="images/ico-menu.png" alt="Icon menu"></a>
+		<div class="womens-landing">
+			<div class="container">
+				<header class="header">
+					<div class="box-legend">
+						<p>Free Shipping on Orders Over $100 + Free Returns. <a href="">Details</a></p>
 					</div>
-					<div class="col col-locate">
-						<a href=""><img src="images/ico-locate.png" alt="Location"></a>
+					<div class="table-header">
+						<div class="col col-menu">
+							<a href=""><img src="images/ico-menu.png" alt="Icon menu"></a>
+						</div>
+						<div class="col col-locate">
+							<a href=""><img src="images/ico-locate.png" alt="Location"></a>
+						</div>
+						<div class="col col-brand">
+							<a href=""><img src="images/brand.png" alt="Johnston & Murphy"></a>
+						</div>
+						<div class="col col-search">
+							<a href=""><img src="images/ico-search.png" alt="Search"></a>
+						</div>
+						<div class="col col-bag">
+							<img src="images/ico-bag.png" alt="Bag">
+						</div>
 					</div>
-					<div class="col col-brand">
-						<a href=""><img src="images/brand.png" alt="Johnston & Murphy"></a>
-					</div>
-					<div class="col col-search">
-						<a href=""><img src="images/ico-search.png" alt="Search"></a>
-					</div>
-					<div class="col col-bag">
-						<img src="images/ico-bag.png" alt="Bag">
-					</div>
-				</div>
-			</header>
-		</div>
-		<div class="container">
-			
-			<!--BOX 1-->
-			<section class="box-1">
-				<div class="caption">
-					<h1>START WITH <br>THE SHOES</h1>
-					<p>Foundations for <br>a Well-Lived Life</p>
-				</div>
-			</section>
-			
-			<!--BOX 2-->
-			<section class="box-2">
-				<div class="box-data">
-					<h2>FOOTWEAR FIRST</h2>
-					<p>Our style philosophy starts from the ground up.</p>
-					<p>Building a timeless wardrobe around classic, versatile pieces that you can dress up or down, transcending trends and giving you a foundation of effortless style that lasts.</p>
-					<p>We believe when you’re on solid footing, the rest comes easy.</p>
-				</div>
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-2.jpg" alt="Footwear First"></a>
-				</div>
-			</section>
-
-			<!--BOX 3-Video-->
-			<section class="box-3-video">
-				<div class='embed-container'><iframe src='https://player.vimeo.com/video/173527809' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-			</section>
-			
-			<!--BOX 4-->
-			<section class="box-4" id="only-mobile">
-				<div class="caption">
-					<small>style philosophy</small>
-					<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
-				</div>
-			</section>
-
-			<!--BOX 5-->
-			<section class="box-5">
-				<div class="box-image-mobile">
-					<a href="#" class="image-link"><img src="images/box-5.jpg" alt=""></a>
-				</div>
-				<div class="box-data">
-					<h2>A Chic-er Sneaker</h2>
-					<p>Multi-task in style. The effortless go-to for the girl on the go.</p>
-					<a href="" class="btn">SHOP THE EMERSON</a>
-				</div>
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-5.jpg" alt="A Chic-er Sneaker"></a>
-				</div>
-			</section>
-			<section class="box-5-images">
-				<div class="box-two-img first">
-					<a href="#" class="image-link"><img src="images/box-5-2.jpg" alt="A Chic-er Sneaker"></a>
-				</div>
-				<div class="box-two-img last">
-					<a href="#" class="image-link"><img src="images/box-5-3.jpg" alt="A Chic-er Sneaker"></a>
-				</div>
-			</section>
-			
-			<!--BOX 4-->
-			<section class="box-4" id="only-desktop">
-				<div class="caption">
-					<small>style philosophy</small>
-					<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
-				</div>
-			</section>
-
-			<!--BOX 6-->
-			<section class="box-6">
-				<div class="col middle only-mobile">
-					<h2>A Well-Balanced <br>Wardrobe</h2>
-					<p>All the rest of the fall essentials you need for toe-to-head style.</p>
-					<a href="#" class="btn">SHOP WOMEN'S APPAREL</a>
-				</div>
-				<div class="col">
-					<a href="#" class="image-link"><img src="images/box-6-1.jpg" alt="A Well-Balanced Wardrobe"></a>
-				</div>
-				<div class="col middle only-desktop">
-					<h2>A Well-Balanced <br>Wardrobe</h2>
-					<p>All the rest of the fall essentials you need for toe-to-head style.</p>
-					<a href="#" class="btn">SHOP WOMEN'S APPAREL</a>
-				</div>
-				<div class="col last">
-					<a href="#" class="image-link"><img src="images/box-6-2.jpg" alt="A Well-Balanced Wardrobe"></a>
-				</div>
-			</section>
-
-			<!--BOX 7-->
-			<section class="box-7">
-				<div class="box-data">
-					<h2>Polish Makes <br>Perfect</h2>
-					<p>Patent, menswear-inspired looks let your feminine style shine.</p>
-					<a href="" class="btn">SHOP BEVERLY & GWYNN</a>
-					<a href="#" class="image-link"><img src="images/box-7.jpg" alt="Polish Makes Perfect"></a>
-				</div>
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-7-2.jpg" alt="Polish Makes Perfect"></a>
-				</div>
-			</section>
-
-			<!--BOX 8-->
-			<section class="box-8">
-				<div class="caption">
-					<small>Introducing</small>
-					<h2>HAPPILY GREY</h2>
-					<a href="#" class="image-link"><img src="images/box-8-mobile.jpg" alt="Happily Grey" class="mobile"></a>
-					<p>We caught up with the style blogger and hometown Nashville girl crush to find out her perspective on life, style, and of course, the shoes.</p>
-					<a href="" class="btn">READ MORE</a>
-				</div>
-				<a href="#" class="image-link"><img src="images/box-8.jpg" alt="Happily Grey" class="desktop"></a>
-			</section>
-
-			<!--BOX 9-->
-			<section class="box-9">
-				<div class="box-image">
-					<a href="#" class="image-link"><img src="images/box-9.jpg" alt="Well Heeled"></a>
-				</div>
-				<div class="box-data">
-					<h2>Well Heeled</h2>
-					<p>Finely crafted from start to luxurious finish.</p>
-					<a href="" class="btn">SHOP VALERIE & VANESSA</a>
-				</div>
+				</header>
+			</div>
+			<div class="container">
 				
-			</section>
-			<section class="box-9-images">
-				<div class="box-two-img first">
-					<a href="#" class="image-link"><img src="images/box-9-1.jpg" alt="Well Heeled"></a>
-				</div>
-				<div class="box-two-img last">
-					<a href="#" class="image-link"><img src="images/box-9-2.jpg" alt="Well Heeled" class="mobile"></a>
-					<a href="#" class="image-link"><img src="images/box-9-3.jpg" alt="Well Heeled" class="desktop"></a>
-				</div>
-			</section>
-			
+				<!--BOX 1-->
+				<section class="box-1">
+					<div class="caption">
+						<h1>START WITH <br>THE SHOES</h1>
+						<p>Foundations for <br>a Well-Lived Life</p>
+					</div>
+				</section>
+				
+				<!--BOX 2-->
+				<section class="box-2">
+					<div class="box-data">
+						<h2>FOOTWEAR FIRST</h2>
+						<p>Our style philosophy starts from the ground up.</p>
+						<p>Building a timeless wardrobe around classic, versatile pieces that you can dress up or down, transcending trends and giving you a foundation of effortless style that lasts.</p>
+						<p>We believe when you’re on solid footing, the rest comes easy.</p>
+					</div>
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-2.jpg" alt="Footwear First"></a>
+					</div>
+				</section>
+
+				<!--BOX 3-Video-->
+				<section class="box-3-video">
+					<div class='embed-container'><iframe src='https://player.vimeo.com/video/173527809' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+				</section>
+				
+				<!--BOX 4-->
+				<section class="box-4" id="only-mobile">
+					<div class="caption">
+						<small>style philosophy</small>
+						<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
+					</div>
+				</section>
+
+				<!--BOX 5-->
+				<section class="box-5">
+					<div class="box-image-mobile">
+						<a href="#" class="image-link"><img src="images/box-5.jpg" alt=""></a>
+					</div>
+					<div class="box-data">
+						<h2>A Chic-er Sneaker</h2>
+						<p>Multi-task in style. The effortless go-to for the girl on the go.</p>
+						<a href="" class="btn">SHOP THE EMERSON</a>
+					</div>
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-5.jpg" alt="A Chic-er Sneaker"></a>
+					</div>
+				</section>
+				<section class="box-5-images">
+					<div class="box-two-img first">
+						<a href="#" class="image-link"><img src="images/box-5-2.jpg" alt="A Chic-er Sneaker"></a>
+					</div>
+					<div class="box-two-img last">
+						<a href="#" class="image-link"><img src="images/box-5-3.jpg" alt="A Chic-er Sneaker"></a>
+					</div>
+				</section>
+				
+				<!--BOX 4-->
+				<section class="box-4" id="only-desktop">
+					<div class="caption">
+						<small>style philosophy</small>
+						<h2>THINKING ON <br> YOUR FEET IS <i>ALWAYS</i> <br> APPROPRIATE</h2>
+					</div>
+				</section>
+
+				<!--BOX 6-->
+				<section class="box-6">
+					<div class="col middle only-mobile">
+						<h2>A Well-Balanced <br>Wardrobe</h2>
+						<p>All the rest of the fall essentials you need for toe-to-head style.</p>
+						<a href="#" class="btn">SHOP WOMEN'S APPAREL</a>
+					</div>
+					<div class="col">
+						<a href="#" class="image-link"><img src="images/box-6-1.jpg" alt="A Well-Balanced Wardrobe"></a>
+					</div>
+					<div class="col middle only-desktop">
+						<h2>A Well-Balanced <br>Wardrobe</h2>
+						<p>All the rest of the fall essentials you need for toe-to-head style.</p>
+						<a href="#" class="btn">SHOP WOMEN'S APPAREL</a>
+					</div>
+					<div class="col last">
+						<a href="#" class="image-link"><img src="images/box-6-2.jpg" alt="A Well-Balanced Wardrobe"></a>
+					</div>
+				</section>
+
+				<!--BOX 7-->
+				<section class="box-7">
+					<div class="box-data">
+						<h2>Polish Makes <br>Perfect</h2>
+						<p>Patent, menswear-inspired looks let your feminine style shine.</p>
+						<a href="" class="btn">SHOP BEVERLY & GWYNN</a>
+						<a href="#" class="image-link"><img src="images/box-7.jpg" alt="Polish Makes Perfect"></a>
+					</div>
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-7-2.jpg" alt="Polish Makes Perfect"></a>
+					</div>
+				</section>
+
+				<!--BOX 8-->
+				<section class="box-8">
+					<div class="caption">
+						<small>Introducing</small>
+						<h2>HAPPILY GREY</h2>
+						<a href="#" class="image-link"><img src="images/box-8-mobile.jpg" alt="Happily Grey" class="mobile"></a>
+						<p>We caught up with the style blogger and hometown Nashville girl crush to find out her perspective on life, style, and of course, the shoes.</p>
+						<a href="" class="btn">READ MORE</a>
+					</div>
+					<a href="#" class="image-link"><img src="images/box-8.jpg" alt="Happily Grey" class="desktop"></a>
+				</section>
+
+				<!--BOX 9-->
+				<section class="box-9">
+					<div class="box-image">
+						<a href="#" class="image-link"><img src="images/box-9.jpg" alt="Well Heeled"></a>
+					</div>
+					<div class="box-data">
+						<h2>Well Heeled</h2>
+						<p>Finely crafted from start to luxurious finish.</p>
+						<a href="" class="btn">SHOP VALERIE & VANESSA</a>
+					</div>
+					
+				</section>
+				<section class="box-9-images">
+					<div class="box-two-img first">
+						<a href="#" class="image-link"><img src="images/box-9-1.jpg" alt="Well Heeled"></a>
+					</div>
+					<div class="box-two-img last">
+						<a href="#" class="image-link"><img src="images/box-9-2.jpg" alt="Well Heeled" class="mobile"></a>
+						<a href="#" class="image-link"><img src="images/box-9-3.jpg" alt="Well Heeled" class="desktop"></a>
+					</div>
+				</section>
+				
+			</div>
 		</div>
 	</main>
 	<!--JQUERY -->
