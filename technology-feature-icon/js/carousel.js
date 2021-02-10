6a4d821f936d7b6913956b0280292cdb6af5d728
Daniela <daniela@bytery.com>
2021-02-10 18:34:52 -0300

    Technology Feature Icon

---
+++ technology-feature-icon/js/carousel.js
@@ -0,0 +1,12 @@
+// WIDGET CAROUSEL 
+
+$(document).ready(function() {
+  var sync1 = $("#carousel-mobile");
+  sync1.owlCarousel({
+    margin:10,
+    autoWidth:true,
+    nav:false,
+  });
+
+});
+
