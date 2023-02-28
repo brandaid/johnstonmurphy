dd55cd8832eb24879fdbbcffaea6f047858cf1bc
Daniela <daniela@bytery.com>
2023-02-28 14:48:15 -0300

    February 2023 Daxton Landing and February 2023 Conard Landing

---
+++ February-2023-Conard-Landing/js/carousel.js
@@ -0,0 +1,21 @@
+// WIDGET CAROUSEL 
+
+$(document).ready(function() {
+  var sync1 = $(".carousel-mobile");
+  sync1.owlCarousel({
+    margin: 20,
+    items: 4,
+    autoWidth:true,
+	loop: true,
+	autoplay: true,
+    nav: false,
+    responsive : {
+    // breakpoint from 768 up
+    768 : {
+		autoWidth:false,
+    }
+}
+  });
+
+});
+
